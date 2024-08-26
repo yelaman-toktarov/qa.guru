d8048a32d6c1e489dae951934bbc502da323f9c3
YELAMAN TOKTAROV <e.toktarov@p-s.kz>
2024-08-27 01:55:47 +0500

    добавил пробел

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
